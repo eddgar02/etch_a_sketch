db7a90bb82079bc2c0bfc8307593d3fe3f858a90
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-02 21:25:54 -0500

    Finish functionality of grid with squares adjusting to size of grid.

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+let userInput = document.querySelector("#userInput");
+let btn = document.querySelector("#mybutton");
 let numberOfGrids = 256;
-const grid  = document.querySelector(".container");
+const grid = document.querySelector(".container");
 
-for(let i = 0;i<numberOfGrids;i++){
-    const sq = document.createElement("div");
-    sq.classList.add("square");
+btn.addEventListener("click", () => {
+    let gridSize = userInput.value;
+    if(gridSize < 1){
+        gridSize = 1;
+    }else if(gridSize > 100){
+        gridSize = 100;
+    }
+    generateGrid(gridSize);
+});
 
-    sq.addEventListener("mouseenter", () =>{
-        sq.style.background = 'white';
-    });
-    
-    grid.appendChild(sq);
+generateGrid(16);
+
+function generateGrid(size) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    for (let i = 0; i < size*size; i++) {
+        const sq = document.createElement("div");
+        sq.style.flex = `0 0 ${100/size}%`;
+        sq.classList.add("square");
+        grid.appendChild(sq);
+
+        sq.addEventListener("mouseenter", () => {
+            sq.style.background = 'white';
+        });
+       
+
+        
+    }
 }
+
 
 
 
